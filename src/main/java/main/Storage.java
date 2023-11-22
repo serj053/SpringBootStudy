0460cd617777b2f6fa6c3bf561e8e695d6aa8b5f
Serj <serj053@yandex.ru>
2023-11-22 23:09:04 +0300

    созан класс Storage и его методы addAllBook и addBook

--- src/main/java/main/Storage.java
+++ src/main/java/main/Storage.java
@@ -1,2 +1,21 @@
-package main;public class Storage {
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Storage {
+    static int currentId = 1;
+    static HashMap<Integer, Book> books = new HashMap<Integer, Book>();
+    public static List<Book> getAllBook(){
+        ArrayList<Book> list = new ArrayList<>();
+        list.addAll(books.values());
+        return list;
+    }
+    public static int addBook(Book book){
+        int id = currentId++;
+        book.setId(id);
+        books.put(id, book);
+        return id;
+    }
 }
