0faff2aa562f9f1e5accb5ee20423afe80760a9a
Serj <serj053@yandex.ru>
2023-11-22 23:06:02 +0300

    создан метод saveBook()

--- src/main/java/main/BookController.java
+++ src/main/java/main/BookController.java
@@ -1,2 +1,14 @@
-package main;public class BookController {
+package main;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    @PostMapping("/books/")
+    public int saveBook(Book book){
+        return Storage.addBook(book);
+    }
 }
