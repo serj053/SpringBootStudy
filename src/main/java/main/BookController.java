44cf02014679aa05ae644efe0e00176397721ace
Serj <serj053@yandex.ru>
2023-11-23 16:06:10 +0300

    add bookRepository and his methods

--- src/main/java/main/BookController.java
+++ src/main/java/main/BookController.java
@@ -1,6 +1,8 @@
 package main;
 
 import main.model.Book;
+import main.model.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,15 @@ import java.util.logging.Logger;
 
 @RestController
 public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
     ;
     @PostMapping("/books/")
     public int saveBook(Book book){
+        Book newBook = bookRepository.save(book);
         Logger.getLogger(BookController.class.getName()).info("*** "+book.toString());
-        return Storage.addBook(book);
+        Storage.addBook(book);
+        return newBook.getId();
     }
     @GetMapping("/books/")
     public List<Book> gatAllBook(){
