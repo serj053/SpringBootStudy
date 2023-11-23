77e9a45c73d72dcf441212e31533dfb04062140c
Serj <serj053@yandex.ru>
2023-11-23 19:30:58 +0300

    Добавлен bookRepository в метод getAllBook

--- src/main/java/main/BookController.java
+++ src/main/java/main/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -16,19 +17,28 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
     ;
+
     @PostMapping("/books/")
-    public int saveBook(Book book){
+    public int saveBook(Book book) {
         Book newBook = bookRepository.save(book);
-        Logger.getLogger(BookController.class.getName()).info("*** "+book.toString());
+        Logger.getLogger(BookController.class.getName()).info("*** " + book.toString());
         Storage.addBook(book);
         return newBook.getId();
     }
+
     @GetMapping("/books/")
-    public List<Book> gatAllBook(){
-        return Storage.getAllBook();
+    public List<Book> gatAllBook() {
+        Iterable<Book> booksIterable = bookRepository.findAll();
+        ArrayList<Book> books = new ArrayList<>();
+        for (Book book : booksIterable) {
+            books.add(book);
+        }
+        return books;//получаем из базы данных
+        //return Storage.getAllBook();//получаем из коллекции(оперативной памяти
     }
+
     @GetMapping("/books/{id}")
-    public Book getBook(@PathVariable int id){
+    public Book getBook(@PathVariable int id) {
         return Storage.getBook(id);
     }
 }
