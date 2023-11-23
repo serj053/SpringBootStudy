58c54f04e3c91e13476f90f6b7a38f9809286620
Serj <serj053@yandex.ru>
2023-11-23 18:16:33 +0300

    добавлен метод запроса GET по id

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,25 @@
 $(function () {
   const addBook = function (data) {
-    var code = '<div>' + data.name + '</div>' +
-      '<div>' + data.year + '</div>';
+    var code = '<div class="get-book" data-id="'
+      + data.id + '">' + data.name + '</div>';
     $('.books').append(code);
   }
 
   $.get('/books/', function (result) {
-    for(i in result){
-       addBook(result[i]);
+    for (i in result) {
+      addBook(result[i]);
     }
-   
-  })
+  });
+
+  $(document).on('click','.get-book',function () {
+    alert("In get-book");
+    var link = $(this);
+    var id = link.data('id');
+    $.get('/books/' + id, function (result) {
+      $(this).after('<div>' + result.year + '</div>');
+    })
+  });
+
   //сохранить на сервере
   $('.save').click(function () {
     var data = $('.form form').serialize();
@@ -19,9 +28,9 @@ $(function () {
       url: '/books/',
       data: data,
       success: function (result) {
-      //  for(i in result) {
-      //   addBook(result[i]);
-      //  }
+        //  for(i in result) {
+        //   addBook(result[i]);
+        //  }
       },
       error: function (jqXHR, textStatus, errorThrown) {
         alert("Error: " + errorThrown);
